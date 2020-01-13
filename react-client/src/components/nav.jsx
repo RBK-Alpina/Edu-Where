9859324273ab043fdc6e1d1ff9791d10db18c443
B.Maher <bouzid.meher@gmail.com>
2020-01-13 12:44:34 +0100

    changed logo color

--- react-client/src/components/nav.jsx
+++ react-client/src/components/nav.jsx
@@ -4,7 +4,7 @@ const NavBar = () => (
   <header className="bg-white w-100 ph3 hover-blackpv3 pv4-ns ph4-m ph5-l bb b--light-gray" style={{ overflow: 'visible', position: 'fixed', top: '0', width: '100%' }}>
     <nav className="tc">
       <div className="fl db dib-ns tc mb3 mb0-ns">
-        <a href="/" title="EduWhere Logo" className="link dim b mr0 mr3-ns f3 ba bw2 b--black" style={{ color: 'blue', borderColor: 'blue', padding: '3px' }}>EdWe</a>
+        <a href="/" title="EduWhere Logo" className="link dim b mr0 mr3-ns f3 ba bw2 b--lightBlue" style={{ color: 'light-blue', borderColor: 'light-blue', padding: '3px' }}>EdWe</a>
       </div>
       <div className='fr'>
         <a className="link dim gray tc f6 f5-ns db dib-ns mr0 mr3-ns mb3 mb0-ns" href="/MyAnnounces" title="MyAnnounces">My Announces</a>
