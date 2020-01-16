19f2ba2431058b498b9c16fe9873207ab73b3ed0
Essam <esamfarg91@gmail.com>
2020-01-16 22:37:40 +0100

    classRoom module done

--- db/models/classRoom.js
+++ db/models/classRoom.js
@@ -1,14 +1,53 @@
 const mongoose = require('mongoose');
-
+require('../index')
 const classRoomSchema = mongoose.Schema({
   name: String,
   description: String,
   categories: String,
-  students: Array,
   teacher: String,
+  students: Array,
   posts: Array
 })
 
 let ClassRoom = mongoose.model('ClassRoom', classRoomSchema)
 
-classRoom.
+module.exports.create = create = (classRoom) => {
+    ClassRoom.create(classRoom)
+    .then(res => {
+      console.log(res)
+      return {
+        status: true,
+        message: 'the class has been created'
+    }
+    })
+    .catch(err => {
+      return {
+        status: false,
+        message: 'failed to create the class'
+      }
+    })
+}
+
+module.exports.find = find = (objectCriteria, callback = {})=> {
+  return ClassRoom.find(objectCriteria)
+}
+
+module.exports.addStudent = addStudent = ( classRoomId, studentId )=>{
+  ClassRoom.findById({_id: classRoomId})
+  .then(res => {
+    res.students.push(studentId)
+    res.save()
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+// create({name: 'angular', description: 'learn angular in one day', categories: 'angular', teacher: 'Belkhere', students: ['Essam', 'Ali'], posts: [35,535,255]})
+
+// find().then(res => {
+//   console.log(res)
+// })
+
+addStudent('5e20d1a709d6444094a35f25', 'sfd')
