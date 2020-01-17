597705d700158e26c4c94cbf3c724958694e3246
mfmehdi <mfmehdi2@gmail.com>
2020-01-17 18:47:38 +0100

    jwt

--- db/models/teacher.js
+++ db/models/teacher.js
@@ -13,7 +13,7 @@ const teacherSchema = mongoose.Schema({
     type: String
   },
   email: {
-    unique: true,
+    //  unique: true,
     // required: true,
     type: String
   },
@@ -39,6 +39,7 @@ const saveTeacher = async (teacher) => {//teacher is object contain all necessar
   const hashedPassword = await bcrypt.hash(teacher.password, salt);
   teacher.password = hashedPassword
   let newTeacher = new Teacher(teacher);
+
   return newTeacher.save();
 };
 
